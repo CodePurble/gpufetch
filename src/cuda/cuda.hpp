64075cae8510a67b3ea695f2519c78a0b551bf9b
Ramprakash C <49452779+CodePurble@users.noreply.github.com>
2022-04-29 22:24:19 +0530

    [v0.24] add newline ending to `CUDA_DRIVER_START_WARNING`

--- src/cuda/cuda.hpp
+++ src/cuda/cuda.hpp
@@ -2,7 +2,7 @@
 #define __CUDA_GPU__
 
 #include "../common/gpu.hpp"
-#define CUDA_DRIVER_START_WARNING "Waiting for CUDA driver to start..."
+#define CUDA_DRIVER_START_WARNING "Waiting for CUDA driver to start...\n"
 
 struct gpu_info* get_gpu_info_cuda(struct pci_dev *devices, int gpu_idx);
 bool print_gpu_cuda(struct gpu_info* gpu);
